341bc889d77829dd00e698d12a5cccf759db470f
Sebannicus <seba.bambutri@gmail.com>
2025-03-25 17:30:24 -0300

    Avance 25-03-2025

--- finanzas/urls.py
+++ finanzas/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('agregar_transaccion/', views.agregar_transaccion, name='agregar_transaccion'),
     path('listar_transacciones/', views.listar_transacciones, name='listar_transacciones'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html', next_page='index'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
     path('register/', views.register, name='register'),
 ]
